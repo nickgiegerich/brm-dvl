d058aa88b48cbd6f74947394b4608a8c919688e1
Nick Giegerich <nickgiegerich@u.boisestate.edu>
2021-08-18 15:58:29 -0600

    spinner test on server

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import "../styles/globals.css";
 import { AnimateSharedLayout, AnimatePresence } from "framer-motion";
 import { useEffect, useState } from "react";
+import Router from "next/router";
 
 function MyApp({ Component, pageProps, router }) {
   const [pageLoading, setPageLoading] = useState(false);
+  const [initialLoad, setInitialLoad] = useState(true);
 
   useEffect(() => {
     const handleStart = () => {
@@ -13,6 +15,19 @@ function MyApp({ Component, pageProps, router }) {
       setPageLoading(false);
     };
 
+    Router.onRouteChangeComplete = () => {
+      console.log("onRouteChnageStart Triggered");
+    };
+
+    // set a timeout function after we verify that it is initial load
+    if (initialLoad) {
+      const timer = setTimeout(() => {
+        setInitialLoad(false);
+        console.log("it is working");
+      }, 3000);
+      return () => clearTimeout(timer);
+    }
+
     router.events.on("routeChangeStart", handleStart);
     router.events.on("routeChangeComplete", handleComplete);
     router.events.on("routeChangeError", handleComplete);
@@ -21,8 +36,8 @@ function MyApp({ Component, pageProps, router }) {
   return (
     <AnimatePresence exitBeforeEnter>
       <AnimateSharedLayout>
-        {pageLoading ? (
-          <div className="bg-black text-white">loading</div>
+        {pageLoading || initialLoad ? (
+          <div className="loading"><div className="loader"></div></div>
         ) : (
           <Component {...pageProps} key={router.route} />
         )}
